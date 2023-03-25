3532a8cc55ab983cea29c9144bb1424ee5487c5a
Gibexz <andrewetedu@gmail.com>
2023-03-25 10:19:16 +0000

    Task12 Corrections

--- pstr.c
+++ pstr.c
@@ -9,13 +9,13 @@
  */
 void _pstr(stack_t **head, unsigned int count)
 {
-	if (!(*head))
+	(void)count;
+	/*if (!(*head))
 	{
 		fprintf(stderr, "L%u: can't pstr, stack empty\n", count);
 		freeStack(*head);
 		exit(EXIT_FAILURE);
-	}
-
+	}*/
 	while (*head)
 	{
 		if ((*head)->n > 127 || (*head)->n <= 0)
